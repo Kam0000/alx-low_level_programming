8268d31299d98c08770180179c78abe963ab02eb
Michael Agyapong <michaelagyapong2018@gmail.com>
2022-10-13 17:12:24 -0700

    added some files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * The main function prints exactly Programming is like building a multilingual puzzle.
+ * return 0 ends the program with value 0
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
+}
