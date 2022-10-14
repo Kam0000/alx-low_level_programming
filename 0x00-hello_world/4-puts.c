8afccca9b3fba139c707bcb5afd71647742cf90b
Michael Agyapong <michaelagyapong2018@gmail.com>
2022-10-13 17:57:06 -0700

    updated 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-/*
- * The main function prints exactly Programming is like building a multilingual puzzle.
- * return 0 ends the program with value 0
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
-puts("Programming is like building multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
